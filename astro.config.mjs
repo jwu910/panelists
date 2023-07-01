48f79088f1d618b8c05c02df013b0b7e0b0545f1
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-07-01 15:02:37 -0700

    chore: use astro again, install more deps, reorg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
@@ -13,7 +12,6 @@ export default defineConfig({
       logLevel: "debug",
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
-    ,
     react(),
   ],
   build: {
--- src/components/JobHistory.tsx
+++ src/components/JobHistory.tsx
@@ -1,4 +1,5 @@
 import TitleAndCompany from "./TitleAndCompany";
+import StyledGradient from "./StyledGradient";
 
 type JobHistory = {
   company: string;
@@ -6,19 +7,30 @@ type JobHistory = {
 };
 
 interface Props {
+  currentEmployer: string;
+  currentTitle: string;
   jobHistory: JobHistory[];
 }
 
 const JobHistory = (props: Props) => {
   return (
-    <ul>
-      {props.jobHistory.map((jobHistory) => (
-        <TitleAndCompany
-          title={jobHistory.title}
-          company={jobHistory.company}
-        />
-      ))}
-    </ul>
+    <>
+      <h2>
+        Currently working as a <StyledGradient text={props.currentTitle} /> at{" "}
+        <StyledGradient text={props.currentEmployer} />
+      </h2>
+      <h2>Tech journey</h2>
+      <ul>
+        {props.jobHistory.map((jobHistory) => (
+          <li>
+            <TitleAndCompany
+              title={jobHistory.title}
+              company={jobHistory.company}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
