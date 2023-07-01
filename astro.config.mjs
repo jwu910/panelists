e0c01fa11fc100db89183403b5340d1b8ab39273
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-06-30 22:29:31 -0700

    chore: add vercel for hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), image(), react()],
+  build: {
+    inlineStylesheets: "auto"
+  },
+  output: "server",
+  adapter: vercel()
 });
