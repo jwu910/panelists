3d80f8efa401575141b933c0cdd0333d4d7dfe1f
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-07-21 20:51:24 -0700

    feat: add new event layout, speakers

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,16 @@
+import type { AstroInstance } from "astro";
+
+export interface ISpeaker extends AstroInstance {
+  events: string[];
+  details: {
+    name: string;
+    currentTitle: string;
+    currentEmployer: string;
+    pronouns: string;
+    profileImage: ImageMetadata;
+  };
+}
+
+export interface IEvent extends AstroInstance {
+  eventName: string;
+}
