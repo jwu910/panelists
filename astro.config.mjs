1525e2e87b128acad078e4f9071173ca5da72b3e
Joshua Wu <Joshua@pepperdata.com>
2024-02-01 19:45:01 -0800

    chore: upgrade astro to latest, fix accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
+// import image from "@astrojs/image";
+// import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
+  // image: {
+  //   service: {
+  //     entrypoint: 'astro/assets/services/sharp',
+  //     config: {
+  //       limitInputPixels: false,
+  //     },
+  //   },
+  // },
   integrations: [
-    mdx(),
-    image({
-      logLevel: "debug",
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+    // mdx(),
+    // image({
+    //   logLevel: "debug",
+    //   serviceEntryPoint: "@astrojs/image/sharp",
+    // }),
     react(),
   ],
   build: {
