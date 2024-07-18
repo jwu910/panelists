d7362bd50bf005f39f8f5112d8f8763b74f732d3
Joshua Wu <code@joshuawu.me>
2024-07-17 23:03:47 -0700

    feat: add github and hosting links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,20 @@ import react from "@astrojs/react";
 // import mdx from "astro/mdx";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [
     // mdx(),
     react(),
+    icon({
+      include: {
+        // Only include following icons in bundle
+        mdi: ["microsoft-internet-explorer", "github"],
+      },
+    }),
   ],
   build: {
     inlineStylesheets: "auto",
